bc3a71ee6dd265bc14db2a4857bc3440eb2f3c6f
lokerd <lokerd5110@yandex.ru>
2022-10-06 21:36:59 +0300

    Header

---
+++ templates/header.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="./templates/css/header.css">
+<div class="header">
+    <div class="container">
+        <div class="left">
+            <div class="logo">
+                <img src="./img/logo.png" alt="Logo">
+            </div>
+            <div class="separ"></div>
+            <div class="alter-page">
+                <a href="#" class="btn anime active">Аниме<span></span></a>
+                <a href="#" class="btn manga">Манга<span></span></a>
+            </div>
+            <div class="separ"></div>
+            <div class="menu">
+                <a href="#" class="btn">Главная</a>
+                <a href="#" class="btn active">Каталог</a>
+                <a href="#" class="btn">Топ-100</a>
+            </div>
+        </div>
+        <div class="right">
+            <button class="search-btn">
+                <img src="./img/search.png" alt="">
+            </button>
+            <div class="profile">
+                <p>Вход в профиль</p>
+                <img src="./img/no-profile.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="line"></div>
+</div>
